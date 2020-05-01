2818a6047a4ec26dbaa8ddd5b885a3d9d77857db
Shashank Shekhar <61483511+shashank0511@users.noreply.github.com>
2020-05-01 22:05:32 +0530

    Array

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -1,71 +1,67 @@
-public class EmployeWage{
-   System.out.println("Welcome to the Employee Wage Computation program");
-//Constants
-   private static final int IS_PART_TIME=1; 
-   private static final int IS_FULL_TIME=2; 
-   private static final int MAX_HRS_IN_MONTH=10; 
-   private static final int EMP_RATE_PER_HR=20; 
-   private  static final int NUM_WORKING_DAYS=20;
+import java.util.Scanner;
+public class EmployeeWage{
 
-	/*
-	Method for calculating monthly wage of employee based on part time/ full time and 
-   calculates salary according to the attendance of the employee as per
-   company name, maximum woking days, wage per hour, maximum hours
-   */
-	public static int caclEmpWageForCompany(String company,int empRate,int numOfDays,int maxHrs) {
-		//decleartion
-     int empHrs=0, totalEmpHrs=0,totalWorkingDays=0;
-     while(totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
-          totalWorkingDays++; 
-          int attendance=(int)(Math.random()*3); 
-          switch(attendance) 
-          { case IS_FULL_TIME:
-            System.out.println("FUll TIME EMPLOYEE");
-            empHrs=8;
-          break; 
-          case IS_PART_TIME:
-           System.out.println("PART TIME EMPLOYEE");
-          empHrs=4;
-          break;
-          default:empHrs=0; 
-          break; 
-		 totalEmpHrs=(totalEmpHrs+empHrs); 
-          System.out.println("Days= "+ totalWorkingDays+ " Emp Hr: "+empHrs);
-      }
-          int totalEmpWage=(totalEmpHrs*empRate);
-           System.out.println("Total Emp Wage for Company "+company+" is ="+totalEmpWage );
-          
-      return totalEmpWage;
-      
-		 
-         while(totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
-      {
-         totalWorkingDays++; 
-         //checking attandance
-         int attendance=(int)(Math.random()*3); 
-         int empHrs=0;
-         switch(attendance) 
-         { case 1:
-            empHrs=8; 
-            break; 
-         case 2:
-            empHrs=4;
-            break;
-         default:
-            empHrs=0;
-            break; 
-         } 
-         totalEmpHr=(totalEmpHr+empHrs);
-      System.out.println("Company: "+company+"No of days worked  "+totalWorkingDays+"No of hours worked  "+empHrs+"Salary for the month: "+totalEmpWage);
-   }
-   //Main method
-   
-      public static void main(String []args){
-      /*Calculating monthly wage for an employee of DMart*/
-      calculateEmpWage("Dmart", 20, 8, 28);
-      /*Calculating monthly wage for an employee of Realince*/
-      calculateEmpWage("Reliance", 30, 8, 28);
-      /*Calculating monthly wage for an employee of Big Market*/
-      calculateEmpWage("BigMarket", 10, 8, 28);
-   }
+
+	public static final double days=20.0;
+	public static void main(String[] args){
+		System.out.println("Welcome to the Employee Wage Computation program");
+		double totalWage;
+		//Declare an Employee objects
+		Employee dmart=new Employee("dMart",20,8,4);
+		Employee Bigmarket=new Employee("Bigmarket",30,9,5);
+	
+
+		//Declare the Array list to store employee objects
+		ArrayList<Employee> companyArrays=new ArrayList<Employee>();
+
+		companyArrays.add(dMart);
+		companyArrays.add(Bigmarket);
+
+
+		//declare array for storing total wages.
+		ArrayList<Double> totalWages=new ArrayList<Double>();
+
+		//declare dictionary for storing values
+		Map companyWagesMap = new HashMap();
+
+		//Attandance
+		for(int i=0; i<companyArrays.size(); i++)
+		{
+			Employee companyEmpWage=companyArrays.get(i);
+			int presence=companyEmpWage.isPresent();
+                	int fullTime=companyEmpWage.isFullTime();
+			switch(presence){
+			case 1:
+				//Check if the employee works fulltime 
+				switch(fullTime){
+				case 1:
+					//Display the wage calculated
+					totalWage=days*companyEmpWage.FtWageCalculation();
+					totalWages.add(totalWage);
+					companyWagesMap.put(companyEmpWage.companyName,totalWage);
+
+				break;
+				case 0:
+					//Display the wage calculated
+                      totalWage=days*companyEmpWage.PtWageCalculation();
+					totalWages.add(totalWage);
+					companyWagesMap.put(companyEmpWage.companyName,totalWage);
+				break;
+				}
+			break;
+
+			case 0:
+				companyWagesMap.put(companyEmpWage.companyName,"Employee absent");
+			break;
+			}
+		}
+
+		System.out.println(companyWagesMap);
+
+		System.out.println("Enter the company you want to query");
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+		System.out.println(companyWagesMap.get(s));
+	}
 }
+
