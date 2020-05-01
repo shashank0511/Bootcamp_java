a4f23c0eee73c0dc56f2820db400b12961e63db1
Shashank Shekhar <61483511+shashank0511@users.noreply.github.com>
2020-05-01 21:40:32 +0530

    Switch Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,40 +3,33 @@ public class EmployeeWage {
 		System.out.println("Welcome to the Employee wage computation");
 		
 	System.out.println("Welcome to the Employee wage computation");
+		
 		int IS_PRESENT=1;
 		double wagePerHour=20;
 		double fullDay=8;
 		double halfDay=4;	
 		double empWage;
-		double monthDays=20;
-		
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		
 		
 		switch ((int) empCheck){
 		case 1:
 			int IS_FULLTIME=1;
 			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-			double totalMonthWage;
+
 			System.out.println("Employee is present");
 
 			switch ((int) fullTimeCheck){
 			case 1:
 				empWage=wagePerHour*fullDay;
-				totalMonthWage=empWage*monthDays;
 				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
-				System.out.println("Employee wage for a month is: "+ totalMonthWage);
 
 			break;
 
-			case 0:
+			default:
 				empWage=wagePerHour*halfDay;
-				totalMonthWage=empWage*monthDays;
-                                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
-				System.out.println("Employee wage for a month is: "+ totalMonthWage);
+                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
 			break;
 			}
-		break;
 
 		case 0:
 			System.out.println("Employee is absent");
@@ -44,3 +37,4 @@ public class EmployeeWage {
 
 	}
 }
+
