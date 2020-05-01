de24d36d1693b4dae1fbfd253f9e496e43002897
Shashank Shekhar <61483511+shashank0511@users.noreply.github.com>
2020-05-01 21:50:50 +0530

    monthly wage

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -2,17 +2,33 @@ public class EmployeWage{
 public static void main(String[] args) 
     {
      System.out.println("Welcome to the Employee wage computation program " );    	
-     		// checking attandance
-     		int IS_PRESENT=1;
-		
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
+       // decleartion
+	//decleartion
+	   private static int isPartTime=1; 
+	   private static int isFullTime=2; 
+	   private static int empRatePerHr=20; 
+	   private static int empHrs=0;
+	   private static int attendance=(int)(Math.random()*3); 
+	   private static int salary=0;
+		//checking attandance
+		switch(attendance)
+		{ 
+		//checking part time /full rime
+		case 1:
+			empHrs=8; 
+		  break;
+		case 2: 
+			empHrs=4; 
+			break;
+		default: 
+			empHrs=0; 
+			break; 
 		}
-		else{
-			System.out.println("Employee is absent");
-		}
-	}
-}
+		//calculating salary of an employee
+		salary=empHrs*empRatePerHr ;
 
+		System.out.println("Salary Of Employee= "+salary );
+
+     }
+}
   
