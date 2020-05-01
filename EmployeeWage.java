f37f84ef6598185c82226574bb2b3795d4639654
Shashank Shekhar <61483511+shashank0511@users.noreply.github.com>
2020-05-01 21:32:58 +0530

    Attandance

---
+++ EmployeeWage.java
@@ -0,0 +1,20 @@
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee wage computation");
+		
+		
+		int IS_PRESENT=1;
+		
+		double empCheck=Math.floor(Math.random() * 10 ) %2;
+		if (empCheck==IS_PRESENT){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is absent");
+		}
+	}
+}
+
+
+
+
